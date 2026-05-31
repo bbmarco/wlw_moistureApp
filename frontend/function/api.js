ac94365b768bf489d45459d07a7e4a1a21dee67c
bbmarco <m.l.baumann@hotmail.ch>
2026-05-31 12:15:58 +0100

    Abstimmung frontend auf backend

--- frontend/function/api.js
+++ frontend/function/api.js
@@ -6,7 +6,8 @@
  */
 
 // Ermittelt dynamisch die Server-Adresse (erlaubt den Betrieb auf PC und Raspberry Pi)
-const serverUrl = window.location.origin;
+//const serverUrl = window.location.origin;
+const serverUrl = "http://192.168.0.52:3000";
 
 /**
  * Holt die aktuellen Sensordaten für eine bestimmte Sensor-ID vom Express-Server.
@@ -15,14 +16,17 @@ const serverUrl = window.location.origin;
  */
 export const fetchSensorData = async (sensorId) => {
   try {
-    const response = await fetch(`${serverUrl}/get-data/${sensorId}`);
+    const response = await fetch(`${serverUrl}/get-data`);
 
     // Validierung: Wenn der Sensor im Backend (noch) nicht existiert
     if (!response.ok) {
       throw new Error(`Sensor ${sensorId} nicht gefunden`);
     }
 
-    return await response.json();
+    const allData = await response.json();
+    const sensorData = allData.find(plant => plant.name === sensorId);
+    
+    return sensorData || null;
   } catch (error) {
     console.error(
       `Fehler beim Abrufen der Daten für Sensor ${sensorId}:`,
